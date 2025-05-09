ea1610d9b5182619ce07a4add036c4fd8119706d
elmerspesia <elmer.dotti@spesia.com.br>
2025-05-09 09:50:48 -0300

    Update 1_De-Anonimizador.py

--- .pages/1_De-Anonimizador.py
+++ .pages/1_De-Anonimizador.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
-import json
 from io import BytesIO
-from anon_utils.anonymizer import deanonymize_data
+import utils
 
 st.set_page_config(page_title="De-Anonimizador", layout="wide")
 
@@ -11,42 +10,36 @@ st.sidebar.markdown("## De-Anonimizador")
 
 st.title("🔓 De-Anonimizador de Dados")
 
-uploaded_data = st.file_uploader("Selecione o arquivo anonimizado", type=["csv", "xlsx", "txt"])
-uploaded_map = st.file_uploader("Selecione o arquivo de mapeamento (JSON)", type=["json"])
+uploaded_file = st.file_uploader("Selecione o arquivo anonimizado", type=["csv", "xlsx", "txt"])
 
-df = None
-
-if uploaded_data:
-    _, ext = uploaded_data.name.split(".")
+if uploaded_file:
+    ext = uploaded_file.name.split(".")[-1].lower()
     if ext == "csv":
-        df = pd.read_csv(uploaded_data)
+        df = pd.read_csv(uploaded_file)
     elif ext == "xlsx":
-        df = pd.read_excel(uploaded_data)
+        df = pd.read_excel(uploaded_file)
     elif ext == "txt":
-        df = pd.read_csv(uploaded_data, sep="\t")
+        df = pd.read_csv(uploaded_file, sep="\t")
+    else:
+        st.error("Formato não suportado.")
+        st.stop()
 
-if df is not None and uploaded_map:
-    try:
-        mapping_dict = json.load(uploaded_map)
-        st.dataframe(df)
-        if st.button("Desanonimizar"):
-            with st.spinner("Revertendo anonimização..."):
-                recovered_df = deanonymize_data(df, mapping_dict)
-                st.success("Dados desanonimizados com sucesso.")
+    st.subheader("Pré-visualização dos dados anonimados")
+    st.dataframe(df)
+
+    if st.button("Desanonimizar"):
+        with st.spinner("Desanonimizando dados..."):
+            try:
+                recovered_df = utils.deanonymize_data(df)
+                st.success("Desanonimização concluída.")
                 st.dataframe(recovered_df)
 
                 buffer = BytesIO()
                 export_format = st.selectbox("Formato para download", ["CSV", "XLSX", "TXT"])
-                file_name = st.text_input("Nome do arquivo de saída", value="dados_revertidos")
+                file_name = st.text_input("Nome do arquivo de saída", value="dados_original")
 
-                if export_format == "CSV":
-                    recovered_df.to_csv(buffer, index=False)
-                    st.download_button("📥 Baixar CSV", buffer.getvalue(), f"{file_name}.csv", "text/csv")
-                elif export_format == "XLSX":
-                    recovered_df.to_excel(buffer, index=False)
-                    st.download_button("📥 Baixar XLSX", buffer.getvalue(), f"{file_name}.xlsx", "application/vnd.ms-excel")
-                elif export_format == "TXT":
-                    recovered_df.to_csv(buffer, sep="\t", index=False)
-                    st.download_button("📥 Baixar TXT", buffer.getvalue(), f"{file_name}.txt", "text/plain")
-    except Exception as e:
-        st.error(f"Erro ao carregar o mapeamento: {e}")
+                utils.export_dataframe(recovered_df, buffer, export_format)
+                st.download_button("📥 Baixar arquivo original", buffer.getvalue(), f"{file_name}.{export_format.lower()}")
+
+            except Exception as e:
+                st.error(f"Erro ao desanonimizar: {e}")
